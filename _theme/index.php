fed10b372a9666e913bfa45c8ecf5e24c881b393
asmi046 <asmi046@gmail.com>
2023-03-28 17:40:31 +0300

    first commit

---
+++ _theme/index.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<section>
+    <div class="_container">
+        <h1>ЧУ ДПО «УЦ Выстрел-Курск»</h1>
+        <p>305038 г.Курск ул.К.Воробьева зд.2Д</p>
+        <p>ИНН: 4632295081</p>
+        <p>ОГРНИП: 1234600000865</p>
+        <p>Сайт находится в разработке, по всем вопросам обращайтесь: <br/> <a href="mailto:vistrelcentr46@yandex.ru">vistrelcentr46@yandex.ru</a></p>
+    </div>
+</section>
+<?php get_footer(); ?>
